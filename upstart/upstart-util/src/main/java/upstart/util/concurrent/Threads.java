90030204d487dc6a14803aa0ebe0512605bce951
josh gruenberg <joshgr@gmail.com>
2022-05-25 14:40:40 -0600

    introduce UPSTART_ENV=DEV inference when run from intellij

--- upstart/upstart-util/src/main/java/upstart/util/concurrent/Threads.java
+++ upstart/upstart-util/src/main/java/upstart/util/concurrent/Threads.java
@@ -1,11 +1,13 @@
 package upstart.util.concurrent;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.Throwables;
 import upstart.util.exceptions.UncheckedInterruptedException;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.time.Duration;
+import java.util.List;
 
 public class Threads {
   public static void sleep(Duration duration) {
@@ -22,5 +24,17 @@ public class Threads {
 
   public static String formatThreadDump() {
     return Joiner.on("\n").join(getThreadDump());
+  }
+
+  /**
+   * Returns the stacktrace of the caller.<p/>
+   *
+   * NOTE: While attempting to be as efficient as possible, this method is probably still rather costly, so it is
+   * probably inappropriate to use from high-traffic call-sites.
+   * @see Throwables#lazyStackTrace
+   */
+  public static List<StackTraceElement> currentStackTrace() {
+    List<StackTraceElement> fromHere = Throwables.lazyStackTrace(new RuntimeException());
+    return fromHere.subList(1, fromHere.size());
   }
 }
--- upstart/upstart/src/main/java/upstart/UpstartStaticInitializer.java
+++ upstart/upstart/src/main/java/upstart/UpstartStaticInitializer.java
@@ -1,8 +1,15 @@
 package upstart;
 
 import org.kohsuke.MetaInfServices;
+import upstart.config.UpstartEnvironment;
+import upstart.util.Ambiance;
+import upstart.util.concurrent.Threads;
 
+import java.lang.management.ManagementFactory;
+import java.util.List;
 import java.util.ServiceLoader;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 /**
  * A base-class for services which must be initialized ASAP at system-startup. Subclasses must be registered via
@@ -21,10 +28,14 @@ import java.util.ServiceLoader;
  * }</pre>
  */
 public abstract class UpstartStaticInitializer {
+  public static final String DEFAULT_DEV_ENVIRONMENT_NAME = "DEV";
+
   static {
+    prepareIntellijDevEnvironment();
     ServiceLoader.load(UpstartStaticInitializer.class)
             .forEach(UpstartStaticInitializer::initialize);
   }
+
 
   public static void ensureInitialized() {
     // nothing to do now, handled by static-initializer above
@@ -32,5 +43,36 @@ public abstract class UpstartStaticInitializer {
 
   public void initialize() {
 
+  }
+
+  public static void prepareIntellijDevEnvironment() {
+    if (inferIntellijDevEnvironment()) {
+      List<StackTraceElement> stackTrace = Threads.currentStackTrace();
+      AtomicBoolean first = new AtomicBoolean(true);
+      System.err.printf(
+              """
+              ******************************************************************************************
+              ***  %s: Detected IntelliJ runtime, assuming %s=%s
+              ***     at %s
+              ******************************************************************************************
+              """,
+              UpstartStaticInitializer.class.getSimpleName(),
+              UpstartEnvironment.UPSTART_ENVIRONMENT,
+              DEFAULT_DEV_ENVIRONMENT_NAME,
+              stackTrace.stream().skip(1)
+                      .filter(ste -> first.getAndSet(false) || !ste.getClassName().startsWith("upstart"))
+                      .limit(3)
+                      .map(Object::toString)
+                      .collect(Collectors.joining("\n***     at "))
+      );
+
+      System.setProperty(UpstartEnvironment.UPSTART_ENVIRONMENT, DEFAULT_DEV_ENVIRONMENT_NAME);
+    }
+  }
+
+  public static boolean inferIntellijDevEnvironment() {
+    return Ambiance.ambientValue(UpstartEnvironment.UPSTART_ENVIRONMENT).isEmpty()
+            && ManagementFactory.getRuntimeMXBean().getInputArguments().stream()
+            .anyMatch(s -> s.contains("JetBrains") || s.contains("IntelliJ"));
   }
 }
