27dea346a616b5ad127aac2a4150f1fc6cce480b
josh gruenberg <joshgr@gmail.com>
2023-04-06 23:56:15 -0600

    intro AsyncHandler, and support with AnnotatedEndpointHandler

---
+++ upstart/upstart-web-javalin/src/main/java/upstart/javalin/AsyncHandler.java
@@ -0,0 +1,19 @@
+package upstart.javalin;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+import upstart.util.concurrent.Promise;
+
+public interface AsyncHandler extends Handler {
+  Promise<?> handleAsync(Context ctx);
+
+  @Override
+  default void handle(@NotNull Context ctx) throws Exception {
+    ctx.future(handleAsync(ctx));
+  }
+
+  default AsyncHandler andThen(AsyncHandler next) {
+    return ctx -> handleAsync(ctx).thenReplaceFuture(() -> next.handleAsync(ctx));
+  }
+}
