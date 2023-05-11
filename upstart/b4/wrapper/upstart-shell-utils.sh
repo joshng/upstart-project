1245d4216a8a2a8b08b6b4f52430d22fbbfd7dde
josh gruenberg <joshgr@gmail.com>
2023-05-10 21:06:38 -0600

    b4 scripts work better with symlinks

--- upstart/b4/wrapper/upstart-shell-utils.sh
+++ upstart/b4/wrapper/upstart-shell-utils.sh
@@ -1,5 +1,12 @@
-set -e -o pipefail
 # utility functions for shell scripts
 absolutepath() {
   echo "$(cd "$(dirname "$1")" && pwd -P)/$(basename "$1")"
+}
+
+delink() {
+  local path="$1"
+  while [[ -L $path ]]; do
+    path="$(readlink "$path")"
+  done
+  echo "$path"
 }
